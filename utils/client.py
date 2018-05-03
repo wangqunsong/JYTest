46efc60ee358b304985c3f97d97c3cc70eba6df8
wangqunsong <18392169938@163.com>
2018-05-03 10:23:43 +0800

    [update]封装client类

--- utils/client.py
+++ utils/client.py
@@ -13,15 +13,17 @@
 
 import requests
 from utils.log import logger
-        
+
+
 class HTTPClient(object):
     def __init__(self):
-        self.url = {}
-        self.headers = {}
-        self.params = {}
-        self.data = {}
         self.url = None
-        self.files = {}
+        self.merchantNo = None
+        self.jsonEnc = None
+        self.keyEnc = None
+        self.sign = None
+        self.merOrderNo = None
+        self.headers = None
 
     def set_url(self, url):
         if url:
@@ -31,22 +33,31 @@ class HTTPClient(object):
         if header:
             self.headers = header
 
-    def set_params(self, param):
-        if param:
-            self.params = param
+    def set_merchant_no(self, merchantNo):
+        if merchantNo:
+            self.merchantNo = merchantNo
 
-    def set_data(self, data):
-        if data:
-            self.data = data
+    def set_merOrder_no(self, merOrderNo):
+        if merOrderNo:
+            self.merchantNo = merOrderNo
 
-    def set_files(self, file):
-        if file:
-            self.files = file
+    def set_jsonEnc(self, jsonEnc):
+        if jsonEnc:
+            self.jsonEnc = jsonEnc
+
+    def set_keyEnc(self, keyEnc):
+        if keyEnc:
+            self.keyEnc = keyEnc
 
     # http get method
+
     def get(self):
         try:
-            response = requests.get(self.url, params=self.params, headers=self.headers)
+            response = requests.get(
+                self.url,
+                merOrderNo=self.merOrderNo,
+                headers=self.headers,
+            )
             # response.raise_for_status()
             response.encoding = 'utf-8'
             logger.debug('{0} {1}'.format('GET', self.url))
@@ -57,9 +68,17 @@ class HTTPClient(object):
             return None
 
     # http post method
-    def post(self, **kwargs):
+    def post(self):
         try:
-            response = requests.post(url=self.url, headers=self.headers, data=self.data, files=self.files)
+            response = requests.post(
+                url=self.url,
+                headers=self.headers,
+                merchantNo=self.merchantNo,
+                merOrderNo=self.merOrderNo,
+                jsonEnc=self.jsonEnc,
+                keyEnc=self.keyEnc,
+                sign=self.sign
+            )
             # response.raise_for_status()
             response.encoding = 'utf-8'
             logger.debug('{0} {1}'.format('POST', self.url))
